1973901eebc0773c9ebacbf90fc9797cf3e5e083
MichalWroblewskiJava <59879241+MichalWroblewskiJava@users.noreply.github.com>
2020-06-06 10:51:13 +0200

    first

---
+++ src/test/java/com/example/mikroblog/mikroblog/MikroblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mikroblog.mikroblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikroblogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
