e6c1e170bfac0df011918c80426dd7b0263a055f
MichalWroblewskiJava <59879241+MichalWroblewskiJava@users.noreply.github.com>
2020-06-26 15:57:43 +0200

    logout added controller

--- src/main/java/com/example/mikroblog/mikroblog/controller/IndexController.java
+++ src/main/java/com/example/mikroblog/mikroblog/controller/IndexController.java
@@ -7,12 +7,17 @@ import com.example.mikroblog.mikroblog.service.PostService;
 import com.example.mikroblog.mikroblog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 
 @Controller
@@ -31,7 +36,7 @@ public class IndexController {
 
     @GetMapping("/index")
     public String index(Model model) {
-        model.addAttribute("postList",postService.sortPostByDate() );
+        model.addAttribute("postList", postService.sortPostByDate());
         model.addAttribute("newCommentForm", new PostFrom());
         model.addAttribute("newPostForm", new PostFrom());
         return "index";
@@ -45,9 +50,17 @@ public class IndexController {
     }
 
     @GetMapping("/logout")
-    public String logout() {
+    public String logout(HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
         return "index";
     }
-
 }
+
+
+
+
+
 
